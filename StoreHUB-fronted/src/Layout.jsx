d53ad0afbf77c69a01ea849fa8ed9a98f3bed710
shrutyi05 <shruti0573@gmail.com>
2026-07-26 15:44:24 +0530

    Fix sidebar and created makefile

--- StoreHUB-fronted/src/Layout.jsx
+++ StoreHUB-fronted/src/Layout.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const Layout = () => {
   return (
     <div>
-      <Navbar/>
-  
-        <Outlet />
-   <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
